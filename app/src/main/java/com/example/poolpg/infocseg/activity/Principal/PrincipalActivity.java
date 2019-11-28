4da098cb96792b785c0b7c6c9fb9476250ef7dec
poolgiancarlos2012 <twisthel_09@hotmail.com>
2019-11-27 22:05:22 -0500

    Implementando Menu Principal

--- app/src/main/java/com/example/poolpg/infocseg/activity/Principal/PrincipalActivity.java
+++ app/src/main/java/com/example/poolpg/infocseg/activity/Principal/PrincipalActivity.java
@@ -4,11 +4,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.poolpg.infocseg.R;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
 public class PrincipalActivity extends AppCompatActivity {
+
+    GridView androidGridView;
+
+    String[] gridViewString = {
+            "Alram", "Android", "Mobile", "Website", "Profile", "WordPress",
+            "Alram", "Android", "Mobile", "Website", "Profile", "WordPress",
+            "Alram", "Android", "Mobile", "Website", "Profile", "WordPress",
+
+    } ;
+    int[] gridViewImageId = {
+            R.drawable.cierre, R.drawable.cierre, R.drawable.cierre, R.drawable.cierre, R.drawable.cierre, R.drawable.cierre,
+            R.drawable.cierre, R.drawable.cierre, R.drawable.cierre, R.drawable.cierre, R.drawable.cierre, R.drawable.cierre,
+            R.drawable.cierre, R.drawable.cierre, R.drawable.cierre, R.drawable.cierre, R.drawable.cierre, R.drawable.cierre,
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        CustomGridViewActivity adapterViewAndroid = new CustomGridViewActivity(PrincipalActivity.this, gridViewString, gridViewImageId);
+        androidGridView = findViewById(R.id.grid_view_image_text);
+        androidGridView.setAdapter(adapterViewAndroid);
+        androidGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int i, long id) {
+                Toast.makeText(PrincipalActivity.this, "GridView Item: " + gridViewString[+i], Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
